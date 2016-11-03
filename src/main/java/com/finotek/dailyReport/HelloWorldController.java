325a06d376ab604ae892676e8fa0023b89f62299
kdw38 <kdw38@nate.com>
2016-11-03 10:47:17 +0900

    add Controller

---
+++ src/main/java/com/finotek/dailyReport/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.finotek.dailyReport;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public @ResponseBody String hello() {
+		return "Hello, Spring Boot!";
+	}
+}
