3bd2b12747aa65416926c3b12347479fbabb1bcf
user <user@hp>
2016-11-04 14:52:49 +0900

    good

--- src/main/java/com/finotek/ServletInitializer.java
+++ src/main/java/com/finotek/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FinoReportApplication.class);
+		return application.sources(FinoReportApplication.class); 
 	}
 
 }
